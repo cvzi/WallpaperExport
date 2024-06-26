ce8864193145117032ab8555e07b3e46edf9280b
cvzi <cuzi@openmail.cc>
2024-06-26 14:21:33 +0200

    Update gradle/build infrastructure
    * Use kts
    * Use version toml catalogue

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.license.report)
+}
+
+android {
+    namespace = "com.github.cvzi.wallpaperexport"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.github.cvzi.wallpaperexport"
+        versionCode = 7
+        versionName = "1.1.2"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+
+    if (project.hasProperty("keystore")) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(project.property("keystore") as String)
+                storePassword = project.property("keystorepassword") as String
+                keyAlias = project.property("keystorealias") as String
+                keyPassword = project.property("keystorekeypassword") as String
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+            }
+        }
+    } else {
+        signingConfigs {
+            create("release") {
+                storeFile = rootProject.file("mykey.jks")
+                storePassword = "password"
+                keyAlias = "key0"
+                keyPassword = "password"
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.implementation.app)
+}
+
+licenseReport {
+    // Run via `gradlew licenseReleaseReport`
+    generateCsvReport = false
+    generateHtmlReport = true
+    generateJsonReport = false
+    copyHtmlReportToAssets = true
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "WallpaperExport"
+include("app")
