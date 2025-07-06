d26b5f2dbb94cf04558a247c214abb6393d36722
cvzi <cuzi@openmail.cc>
2025-07-06 14:51:11 +0200

    Remove DependencyInfoBlock for F-Droid #107

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,11 @@ android {
         viewBinding = true
         buildConfig = true
     }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
 }
 
 kotlin {
